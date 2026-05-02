abcf333ccf246c93b67e210d386830751d803440
NanaYaguti <annalisiayaguti@gmail.com>
2026-05-02 14:27:54 -0300

    Update programa.py
    adição de classe e lista encadeada e funções da classe. Remoção de cria_lista

--- trabalhoORD/programa.py
+++ trabalhoORD/programa.py
@@ -1,46 +1,87 @@
 from sys import argv
 from struct import pack, unpack, calcsize
 import os
-        
-def cria_lista(nome_arq_entrada: str) -> list: #construção do índice principal :)
-    '''vai criar uma lista e armazenar TODOS os
-    registros do arquivo de entrada.'''
-    registros = []
-    with open(nome_arq_entrada, 'rb') as entrada:
-        tam = entrada.read(2)
-        tam_int = int.from_bytes(tam, 'little')
-        v
+from dataclasses import dataclass
+from copy import deepcopy
 
+@dataclass
+class chave:
+    valor: int
+class no:
+    def __init__(self, x:chave):
+        self.dado: chave = x
+        self.prox: no | None = None
+
+    def vazia(self):
+        '''Verifica se a lista está vazia.
+        False'''
+        return self.primeiro == None
+    
+class lista:
+    def __init__(self):
+        self.primeiro: no | None = None
+        self.ultimo: no | None = None
+    
+    def busca(self, chave:int) -> no:
+        '''Busca um número na lista pela chave. Se ele não estiver na lista,
+        retorna None.
+        '''
+        ptr = self.primeiro
+        while (ptr != None) and (ptr.dado.valor != chave):
+            ptr = ptr.prox
+        return ptr
+    
+    def busca_item(self, chave:int) -> chave | None:
+            '''Busca um item com base na função *busca*, se ela não estiver na lista,
+            retorna None.'''
+            ptr = self.busca(chave)
+            if ptr != None:
+                return deepcopy(ptr.dado)
+            else:
+                return None
+        
+    def insere_fim(self, x:chave) -> bool:
+        '''Insere um novo item no final da lista. Se esse item já estiver na
+        lista, retorna False.'''
+        if self.busca(x.valor) == None:
+            novo = no(x)
+            if self.vazia():
+                self.primeiro = novo
+            else:
+                self.ultimo.prox = novo
+            self.ultimo = novo
+            return True
+        else:
+            return False
+    
 def cria_ind(nome_arq_entrada: str):
     '''Lê o aquivo de entrada e adiciona os registros
     lidos em separações diferentes dependendo a partir
     de chaves (1 primária - id e 2 secundárias - gênero 
     e publicadora)'''
     registros = []
-    list_chave_gen = []
-    list_
+    list_chave_gen = lista()
+    list_chave_prod = lista()
     with open(nome_arq_entrada, 'rb') as entrada:
         offset = 0
         tam_bytes = entrada.read(2)
         tam_int = int.from_bytes(tam_bytes, 'little')
         while tam_int > 0:
-            offset = offset + tam_int + 2
             reg = entrada.read(tam_int)
             reg_str = reg.decode()
             campos = reg_str.split(sep='|')
-            id = campos[0]
+            campos[0] = int(campos[0])
             campos.pop() #elimina o último campo criado pelo split('|'), já que cada bloco termina com '|'
-            registros.append(int(id), tam + reg)
-            tam = entrada.read(2)
-            tam_int = int.from_bytes(tam, "little")
-        registros.sort() #já organiza as listas de jogos pela numeraçã
-        for #a ideia é percorrer a lista gerada para ir adicionando nas outras listas definidas no começo da função (em mente de que essas listas armazenarão outras listas dentro delas)
-
+            registros.append(campos)
+            offset = offset + tam_int + 2
+            tam_bytes = entrada.read(2)
+            tam_int = int.from_bytes(tam_bytes, "little")
+        print(registros)
 def main():
     if len(argv) < 2:
         raise TypeError('Número incorreto de argumentos\nModo de uso: nome_arq_entrada nome_arq_saida')
-    o = cria_lista(argv[1])
-    print(o)
+    cria_ind(argv[1])
+
 
 
 if __name__ == '__main__':
