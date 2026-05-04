0d8d751153ff978b45a3e70328497b47d55f19b6
NanaYaguti <annalisiayaguti@gmail.com>
2026-05-04 09:47:08 -0300

    Update programa.py
    Desconsideração da função da lista inversa, teste das listas secundárias PODE estar certo, mas falta a criação de uma nova varinavel

--- trabalhoORD/programa.py
+++ trabalhoORD/programa.py
@@ -55,15 +55,16 @@ class lista:
         else:
             return False
     
-def constroi_indices():
+def cria_lista(nome_arq_entrada: str):
     '''Lê o aquivo de entrada e adiciona os registros
     lidos em separações diferentes dependendo a partir
     de chaves (1 primária - id e 2 secundárias - gênero 
     e publicadora)'''
-    nome_arq_entrada = "games.dat" #coloquei aqui porque o arquivo vai ser fixo "games.dat", daí pra não precisar receber um arquivo por parâmetro
-    list_chave_prim = [] #lista da chave primária
-    list_chave_gen = []  #lista da chave secundária: gênero
-    list_chave_pub = [] #lista da chave secundária: publicadora
+    list_chave_prim:list[tuple] = [] #lista da chave primária
+    list_chave_gen:list[tuple[str, lista]] = []  #lista da chave secundária: gênero
+    list #lista da chave secundária: gênero
+    list_chave_pub:list[tuple[str, lista]] = [] #lista da chave secundária: publicadora
+    lista_invertida:list[list] = [] #lista invertida
     with open(nome_arq_entrada, 'rb') as entrada:
         tam_bytes = entrada.read(2)
         tam_int = int.from_bytes(tam_bytes, 'little')
@@ -82,7 +83,7 @@ def constroi_indices():
             n = 0
             while n < len(list_chave_gen) and genero_encontrado == False:
                 if genero == list_chave_gen[n][0]:
-                    list_chave_gen[n][1].insere_fim(int(id))
+                    list_chave_gen[n][1].insere_fim(chave(int(id)))
                     genero_encontrado = True
                 n += 1
             if genero_encontrado == False:
@@ -95,31 +96,52 @@ def constroi_indices():
             m = 0
             while m < len(list_chave_pub) and publicadora_encontrada == False:
                 if publicadora == list_chave_pub[m][0]:
-                    list_chave_pub[m][1].insere_fim(int(id))
+                    list_chave_pub[m][1].insere_fim(chave(int(id)))
                     publicadora_encontrada = True
                 m += 1
             if publicadora_encontrada == False:
                 nova_lista_pub = lista()
                 nova_lista_pub.insere_fim(chave(int(id)))
                 list_chave_pub.append([publicadora, nova_lista_pub])
-                
             tam_bytes = entrada.read(2)
-            tam_int = int.from_bytes(tam_bytes, 'little')
+            tam_int = int.from_bytes(tam_bytes, "little")
+        #próximo passo: fazer a lista invertida
+        elementos: list = []
+        elem_gen = 0
+        elem_pub = 0
+        # for elem in list_chave_prim:
+        #     id = elem[0]
+        #     elementos.append(id)
 
+        #     while elem_gen < len(list_chave_gen):
+        #         id_procurado_gen = list_chave_gen[elem_gen][1].busca(chave(id))
+            #     if id_procurado_gen != None:
+            #         if id_procurado_gen.prox != None:
+            #             elementos.append(id_procurado_gen.prox.dado.valor)
+            #         else:
+            #             elementos.append(-1)
+            #     elem_gen += 1
+            # while elem_pub < len(list_chave_pub):
+            #     id_procurado_pub = list_chave_pub[elem_pub][1].busca(chave(id))
+            #     if id_procurado_pub != None:
+            #         if id_procurado_pub.prox != None:
+            #             elementos.append(id_procurado_pub.prox.dado.valor)
+            #         else:
+            #             elementos.append(-1)
+            #     elem_pub += 1
+            # lista_invertida.append(elementos)
+        list_chave_prim.sort()
+    # print(lista_invertida)
+    # print(list_chave_prim)
+    print(list_chave_pub)
+    print(list_chave_gen)                    
+                
 def main():
     if len(argv) < 2:
-        raise TypeError('Número incorreto de argumentos\nModo de uso: python programa.py [ -b | -e arquivo_operacoes | -c')
-    
-    flag = argv[1]
+        raise TypeError('Número incorreto de argumentos\nModo de uso: nome_arq_entrada nome_arq_saida')
+    cria_lista(argv[1])
 
-    if flag == "-b":
-        constroi_indices()
-    elif flag == "-e":
-        if len(argv) < 3:
-            raise TypeError('Número incorreto de argumentos\nModo de uso: python programa.py -e arquivo_operacoes')
-        #executar_operacoes()
-    elif flag == "-c":
-        # compactar()
-        pass
+
+
 if __name__ == '__main__':
     main()
