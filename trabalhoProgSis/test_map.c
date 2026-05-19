aa771f03d16a4f3350d56371c95b54d20ba047ab
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-19 15:48:00 -0300

    Add test_map.c for Map functionality testing

---
+++ trabalhoProgSis/test_map.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "map.h"
+
+int main(){
+    Map *m = map_create(5, 5);
+    assert(m != NULL);
+
+    assert(map_rows(m) == 5);
+
+    assert(map_cols(m) == 5);
+
+    map_set(m, 2, 3, WALL);
+    assert(map_get(m, 2, 3) == WALL);
+    
+    map_set(m, 2, 3, OPEN);
+    assert(map_get(m, 2, 3) == OPEN);
+
+    map_free(m);
+
+    printf("OK!\n");
+    return 0;
+}
