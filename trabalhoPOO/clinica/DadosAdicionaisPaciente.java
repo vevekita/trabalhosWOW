3159794e3aa121fe33a80ff99478bae8a9978e76
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-04 13:18:41 -0300

    Add DadosAdicionaisPaciente class with patient data fields

--- trabalhoPOO/clinica/DadosAdicionaisPaciente.java
+++ trabalhoPOO/clinica/DadosAdicionaisPaciente.java
@@ -1,1 +1,12 @@
+package clinica;
 
+public class DadosAdicionaisPaciente {
+    private int idPaciente;
+    private boolean fuma;
+    private boolean bebe;
+    private boolean colesterol;
+    private boolean diabetes;
+    private boolean doencaCardiaca;
+    private String cirurgias;
+    private String alergias;
+}
