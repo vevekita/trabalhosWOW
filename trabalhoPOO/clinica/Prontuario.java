b5d8d47abae3b08818b858f5b11404e63ca45304
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-04 13:19:15 -0300

    Update Prontuario.java

--- trabalhoPOO/clinica/Prontuario.java
+++ trabalhoPOO/clinica/Prontuario.java
@@ -1,1 +1,9 @@
+package clinica;
 
+public class Prontuario {
+    private int idPaciente;
+    private int idMedico;
+    private String sintomas;
+    private String diagnostico;
+    private String prescricao;
+}
