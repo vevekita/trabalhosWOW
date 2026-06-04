76d22e3b2d506f441292bdbc201904f6a3117a84
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-04 13:18:08 -0300

    Add Consultas class with consultation details

--- trabalhoPOO/clinica/Consultas.java
+++ trabalhoPOO/clinica/Consultas.java
@@ -1,1 +1,9 @@
+package clinica;
 
+public class Consultas {
+    private String data;
+    private String horario;
+    private Medico medico;
+    private Paciente paciente;
+    private String tipoConsulta;
+}
