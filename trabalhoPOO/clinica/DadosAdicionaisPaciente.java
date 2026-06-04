797f5d8ece7d8630ec397e30b19866b1c8358978
NanaYaguti <annalisiayaguti@gmail.com>
2026-06-04 19:55:48 -0300

    Update DadosAdicionaisPaciente.java
    add gets and sets
    add atributo paciente

--- trabalhoPOO/clinica/DadosAdicionaisPaciente.java
+++ trabalhoPOO/clinica/DadosAdicionaisPaciente.java
@@ -1,7 +1,8 @@
 package clinica;
 
+
 public class DadosAdicionaisPaciente {
-    private int idPaciente;
+    private Paciente paciente;
     private boolean fuma;
     private boolean bebe;
     private boolean colesterol;
@@ -9,4 +10,81 @@ public class DadosAdicionaisPaciente {
     private boolean doencaCardiaca;
     private String cirurgias;
     private String alergias;
+    
+    public DadosAdicionaisPaciente() {}
+    public DadosAdicionaisPaciente(Paciente paciente, boolean fuma, boolean bebe, boolean colesterol, boolean diabetes,boolean doencaCardiaca, String cirurgias, String alergias){
+        this.paciente = paciente;
+        this.fuma = fuma;
+        this.bebe = bebe;
+        this.colesterol = colesterol;
+        this.diabetes = diabetes;
+        this.doencaCardiaca = doencaCardiaca;
+        this.cirurgias = cirurgias;
+        this.alergias = alergias;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public boolean isFuma() {
+        return fuma;
+    }
+
+    public void setFuma(boolean fuma) {
+        this.fuma = fuma;
+    }
+
+    public boolean isBebe() {
+        return bebe;
+    }
+
+    public void setBebe(boolean bebe) {
+        this.bebe = bebe;
+    }
+
+    public boolean isColesterol() {
+        return colesterol;
+    }
+
+    public void setColesterol(boolean colesterol) {
+        this.colesterol = colesterol;
+    }
+
+    public boolean isDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(boolean diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public boolean isDoencaCardiaca() {
+        return doencaCardiaca;
+    }
+
+    public void setDoencaCardiaca(boolean doencaCardiaca) {
+        this.doencaCardiaca = doencaCardiaca;
+    }
+
+    public String getCirurgias() {
+        return cirurgias;
+    }
+
+    public void setCirurgias(String cirurgias) {
+        this.cirurgias = cirurgias;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+    
 }
