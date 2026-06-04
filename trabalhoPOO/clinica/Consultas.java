455364ea6ed67636316e2f2525cd69319be6513f
NanaYaguti <annalisiayaguti@gmail.com>
2026-06-04 19:54:30 -0300

    Update Consultas.java
    add sets and gets

--- trabalhoPOO/clinica/Consultas.java
+++ trabalhoPOO/clinica/Consultas.java
@@ -1,9 +1,61 @@
 package clinica;
 
-public class Consultas {
+
+public class Consulta {
     private String data;
     private String horario;
-    private Medico medico;
+    private String medico;
     private Paciente paciente;
-    private String tipoConsulta;
+    private String tipoConsulta; //consulta normal(1h) ou retorno(30min)
+    
+    public Consulta() {
+        
+    }
+    public Consulta(String data, String horario, String medico, Paciente paciente, String tipoConsulta) {
+        this.data = data;
+        this.horario = horario;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.tipoConsulta = tipoConsulta;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public void setTipoConsulta(String tipoConsulta) {
+        this.tipoConsulta = tipoConsulta;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    
+    public String getTipoConsulta() {
+        return tipoConsulta;
+    }
 }
