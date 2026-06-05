1dfd5750bf3d79d940f9e50ce4b93d7affb343e8
NanaYaguti <annalisiayaguti@gmail.com>
2026-06-05 15:09:07 -0300

    Update Prontuario.java
    add getters and setters
    add javadoc

--- trabalhoPOO/clinica/Prontuario.java
+++ trabalhoPOO/clinica/Prontuario.java
@@ -1,9 +1,50 @@
 package clinica;
 
+/**
+ * Classe que armazena as informações clínicas de um paciente:
+ * -IdPaciente (para associação à uma classe Paciente)
+ * -Sintomas
+ * -Diagnostico
+ * -Prescrição
+ * 
+ */
 public class Prontuario {
     private int idPaciente;
-    private int idMedico;
     private String sintomas;
     private String diagnostico;
     private String prescricao;
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public String getSintomas() {
+        return sintomas;
+    }
+
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getPrescricao() {
+        return prescricao;
+    }
+
+    public void setPrescricao(String prescricao) {
+        this.prescricao = prescricao;
+    }
+    
+    
 }
