e2e05b7e803397d799952ce7abf4767052489730
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-06 16:37:11 -0300

    Refactor RepositorioPaciente to use List interface

--- trabalhoPOO/repositorio/RepositorioPaciente.java
+++ trabalhoPOO/repositorio/RepositorioPaciente.java
@@ -1,4 +1,6 @@
 package repositorio;
+
+import java.util.List;
 import java.util.ArrayList;
 import clinica.Paciente;
 
@@ -12,14 +14,28 @@ import clinica.Paciente;
  * - remove paciente(Que remove um paciente da lista)
  * - atualiza x(Atualiza a informação x de determinado paciente na lista, onde x pode ser: Endereço, info de contato ou tipo de convenio)
  */
+
 public class RepositorioPaciente {
-    private final ArrayList<Paciente> pacientes = new ArrayList();
+    private final List<Paciente> pacientes = new ArrayList<>();
             
     public void adicionarPaciente(Paciente paciente){
         pacientes.add(paciente);
     }
-    
+
+    public void removePaciente(Paciente paciente){
+        pacientes.remove(paciente);
+    }
+
     public Paciente buscaPaciente(int idPaciente){
+        for (Paciente p : pacientes) {
+            if (p.getDadoIdentificacao() == idPaciente) {
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    /*public Paciente buscaPaciente(int idPaciente){
         int index = -1;
         Paciente p1 = new Paciente();
         
@@ -37,16 +53,13 @@ public class RepositorioPaciente {
             p1.setDadosIdentificacao(-1);      /*Marcação lógica de que o Paciente não existe, ou seja, o Paciente não está na lista*/
         }
         
-        return p1;
-    }
-    
-    public void removePaciente(Paciente paciente){
-        pacientes.remove(paciente);
-    }
-    
-    public void atualizaDataNascimento(Paciente paciente, String novaData){
+        /*return p1;
+    }*/
+
+    //passei a parte de atualizar pro ServicoSecretaria
+    /*public void atualizaDataNascimento(Paciente paciente, String novaData){
         int index = pacientes.indexOf(paciente);            /*Indice na lista que se encontra o paciente que será atualizado*/
-        pacientes.get(index).setDataNascimento(novaData);
+        /*pacientes.get(index).setDataNascimento(novaData);
     }
     
     public void atualizaEndereco(Paciente paciente, String novoEndereco){
@@ -67,6 +80,6 @@ public class RepositorioPaciente {
     public void atualizaTipoConvenio(Paciente paciente, String novoConvenio){
         int index = pacientes.indexOf(paciente);
         pacientes.get(index).setTipoConvenio(novoConvenio);
-    }
+    }*/
 }
 
