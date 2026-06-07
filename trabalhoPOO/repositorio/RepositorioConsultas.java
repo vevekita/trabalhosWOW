d4eeb49ffcbf533d70624531eeb59a8d49c9e04e
NanaYaguti <annalisiayaguti@gmail.com>
2026-06-07 19:50:35 -0300

    Fix method VerificaColisaoHoras (used polimorphism)
    lobotomia não parece uma má ideia

--- trabalhoPOO/repositorio/RepositorioConsultas.java
+++ trabalhoPOO/repositorio/RepositorioConsultas.java
@@ -15,8 +15,8 @@ import java.time.LocalDate;
  * - remove consulta(Auto explicativo)
  * - Atualiza x(Que atualiza a informação x de uma determinada consulta, podendo ser: Data, Horario, Medico, Paciente ou Tipo de Consulta)
  * 
- * Além disso, essa classe contém um método privado:
- * - verifica colisão de horários(Serve para verificar se ocorrerá uma colisão de horários)
+ * Além disso, essa classe contém um método no qual é utilizado polimorfismo de sobreposição:
+ * - verifica colisão de horários(Serve para verificar se ocorrerá uma colisão de horários), para cadastro e para atualizações(de data, de horario e de tipo de consulta)
  */
 
 public class RepositorioConsultas {
@@ -46,7 +46,7 @@ public class RepositorioConsultas {
         return consultas.get(index);
     }
 
-    public Consulta buscarConsulta(String data, int horas, int minutos, String medico) { // Não tem como um mesmo médico estar em duas consultas diferentes no mesmo horário no mesmo dia
+    public Consulta buscarConsulta(LocalDate data, int horas, int minutos, String medico) { // Não tem como um mesmo médico estar em duas consultas diferentes no mesmo horário no mesmo dia
         for (Consulta c : consultas) {
             if (c.getData().equals(data) && c.getHoras() == horas && c.getMinutos() == minutos && c.getMedico().equals(medico)) {
                 return c; // Retorna a consulta encontrada
@@ -59,6 +59,7 @@ public class RepositorioConsultas {
         consultas.remove(consulta);
     }
     
+    //sobrecarga paa cadastro:
     public boolean verificaColisaoHorarios(int horas, int minutos, LocalDate data, String medico, Paciente paciente, String tipoConsulta){
         boolean ocorreColisao = false;
         
@@ -91,12 +92,55 @@ public class RepositorioConsultas {
                     }
                 }
                 int fimC = horarioC + duracaoC;
-                if (c.getHoras() == horas && c.getMinutos() == minutos && c.getData().equals(data) && c.getMedico().equals(medico) && c.getPaciente().equals(paciente) && c.getTipoConsulta().equals(tipoConsulta)) {  //se a consulta de verificação é igual à consulta na lista:
-                    ocorreColisao = false;
-                    break;
-                }
                 if (c.getData().equals(data)){
                     if (c.getMedico().equals(medico) || c.getPaciente().equals(paciente)) {
+                        if (horarioC < fimCons && fimC > horarioCons) { //verifica sobreposição de horários
+                            ocorreColisao = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ocorreColisao;
+    }
+    //sobrecarga para atualizações:
+    public boolean verificaColisaoHorarios(int horas, int minutos, LocalDate data, String medico, String tipoConsulta, Consulta consulta) {
+        boolean ocorreColisao = false;
+        if (consultas.isEmpty()){
+            ocorreColisao = false;
+        }
+        else{
+            int horarioCons = (horas * 60) + minutos;
+            int duracaoCons = 0;
+            //se o tipo da Consulta for o normal(duração de 1h):
+            if ("normal".equals(tipoConsulta)) {
+                duracaoCons = 60;
+            } else {
+                //se o tipo da Consulta for de retorno (duração de 30min):
+                if ("retorno".equals(tipoConsulta)) {
+                    duracaoCons = 30;
+                }
+            }
+            int fimCons = horarioCons + duracaoCons;
+            
+            for (Consulta c: consultas){
+                if (consulta != null && c == consulta) { //se a consulta selecionada foi encontrada na lista:
+                    continue; //desconsidera as operações dessa consulta específica dentro do loop
+                }
+                int horarioC = (c.getHoras() * 60) + c.getMinutos();
+                int duracaoC = 0;
+                if ("normal".equals(c.getTipoConsulta())) {
+                    duracaoC = 60;
+                } else {
+                    if ("retorno".equals(c.getTipoConsulta())) {
+                        duracaoC = 30;
+                    }
+                }
+                int fimC = horarioC + duracaoC;
+                if (c.getData().equals(data)){
+                    if (c.getMedico().equals(medico) || c.getPaciente().equals(consulta.getPaciente())) {
                         if (horarioC < fimCons && fimC > horarioCons) { //verifica sobreposição de horários
                             ocorreColisao = true;
                             break;
