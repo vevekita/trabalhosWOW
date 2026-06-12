a4771fe2bd34f6263d131468bafa5bc28038ffe1
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-12 16:32:49 -0300

    Update programa.py

--- trabalhoORD2.0/programa.py
+++ trabalhoORD2.0/programa.py
@@ -1,1 +1,28 @@
+from sys import argv
 
+def constroi_indices():
+    pass
+
+def executa_operacoes(arquivo_operacoes: str):
+    pass
+
+def imprime():
+    pass
+
+def main():
+    if len(argv) < 2:
+        raise TypeError('Número incorreto de argumentos\nModo de uso: python programa.py [ -b | -e arquivo_operacoes | -p')
+    
+    flag = argv[1]
+
+    if flag == "-b":
+        constroi_indices()
+    elif flag == "-e":
+        if len(argv) < 3:
+            raise TypeError('Número incorreto de argumentos\nModo de uso: python programa.py -e arquivo_operacoes')
+        executa_operacoes(argv[2])
+    elif flag == "-p":
+        imprime()
+
+if __name__ == '__main__':
+    main()
