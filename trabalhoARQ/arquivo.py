2701b2a0452b4d423346de7b294672b0a66c4887
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-18 23:09:13 -0300

    Refactor registradores and add display function

--- trabalhoARQ/arquivo.py
+++ trabalhoARQ/arquivo.py
@@ -1,5 +1,5 @@
 # ALUNAS: Ana Julia Thibes, RA: 145108; Anna Lísia Yaguti, RA: 145093; Verônica Kitamura, RA: 145096
-# --------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 RAM = [0] * 256
 
@@ -9,9 +9,9 @@ REGS_GERAIS = {
 }
 
 REGS_ESPECIAIS ={
-    'PC': 0x00, # Program Counter (ponteiro de instruções)
+    'PC': 0, # Program Counter (ponteiro de instruções)
     'IR': "", # Instruction Register (guarda a instrução atual)
-    'MAR': 0x00, # Memory Adress Register
+    'MAR': 0, # Memory Adress Register
     'MBR': None, # Memory Buffer Register
     'AC': 0, # Acumulador
     'M': 0, # Multiplicador
@@ -21,6 +21,18 @@ REGS_ESPECIAIS ={
     'N': 0, # Negativo (0 ou 1)
     'Z': 0 # Zero (0 ou 1)
 }
+
+def exibir_registradores():
+    # Função para exibir o estado atual dos registradores na tela inicial do usuário
+    print('ESTADO ATUAL DOS REGISTRADORES')
+    print(f"Gerais   -> A: {REGS_GERAIS['A']} | B: {REGS_GERAIS['B']}")
+    print(f"Controle -> PC: 0x{REGS_ESPECIAIS['PC']:02X} | IR: '{REGS_ESPECIAIS['IR']}'")
+    print(f"Memória  -> MAR: 0x{REGS_ESPECIAIS['MAR']:02X} | MBR: {REGS_ESPECIAIS['MBR']}")
+    print(f"ULA      -> AC: {REGS_ESPECIAIS['AC']} | M: {REGS_ESPECIAIS['M']} | R: {REGS_ESPECIAIS['R']}")
+    print(f"Flags    -> C: {REGS_ESPECIAIS['C']} | N: {REGS_ESPECIAIS['N']} | Z: {REGS_ESPECIAIS['Z']}")
+    print()
+
+    input('Pressione <ENTER> para avançar para o próximo ciclo...')
 
 def executar_comando(instrucao: str): #LOAD A, 0x05
     partes = instrucao.replace(',', ' ').split()
@@ -38,9 +50,26 @@ def executar_comando(instrucao: str): #LOAD A, 0x05
     # pode ter mais operações, só lembrei dessas de cabeça
 
 def ciclo_instrucao():
+    exibir_registradores()
     while True:
-        if RAM[REGS_ESPECIAIS['PC']] == 0 or RAM[REGS_ESPECIAIS['PC']] == '':
+        indice_atual = REGS_ESPECIAIS['PC']
+        # Condição de parada
+        if indice_atual >= 256 or RAM[indice_atual] == 0 or RAM[indice_atual] == '':
             print('Execução finalizada ou fim da memória alcançado!')
             break
 
-        # não terminei hehe
+        # MAR <- PC
+        REGS_ESPECIAIS['MAR'] = REGS_ESPECIAIS['PC']
+        # MBR <- RAM[MAR]
+        REGS_ESPECIAIS['MBR'] = RAM[REGS_ESPECIAIS['MAR']]
+        # IR <- MBR
+        REGS_ESPECIAIS['IR'] = REGS_ESPECIAIS['MBR']
+        # PC <- PC + 1
+        REGS_ESPECIAIS['PC'] += 1
+        # Executar instrução
+        comando = REGS_ESPECIAIS['IR']
+
+        print(f"Executando comando {comando}...")
+        executar_comando(comando)
+
+        exibir_registradores()
