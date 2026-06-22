2842253fe492fd9e6c71becd6d447990bceb103d
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-22 15:50:34 -0300

    Fix RAM initialization and update instruction handling

--- trabalhoARQ/arquivo.py
+++ trabalhoARQ/arquivo.py
@@ -1,7 +1,7 @@
 # ALUNAS: Ana Julia Thibes, RA: 145108; Anna Lísia Yaguti, RA: 145093; Verônica Kitamura, RA: 145096
 #---------------------------------------------------------------------------------------------------
 
-RAM = [None] * 256 # Aparecem erros durante o código por causa da inicialização aqui, mas ainda não descobri como resolver
+RAM = [int, ''] * 256 # Armazena valor inteiro ou instrução (String)
 
 REGS_GERAIS = {
     'A': 0,
@@ -36,8 +36,8 @@ def carregar_arquivo(nome_arquivo):
         return False
     
     modo = None
-    ponteiro_dados = 0      # Dados começam na posição 0x00 da RAM
-    ponteiro_instr = None   # Será definido pelo arquivo (ex: 0xA0)
+    ponteiro_dados = 0
+    ponteiro_instr = 0 # Será definido pelo arquivo (ex: 0xA0)
 
     for linha in linhas:
         linha_limpa = linha.strip()
@@ -66,22 +66,22 @@ def carregar_arquivo(nome_arquivo):
         elif modo == "INSTRUCOES":
             parte_util = linha_limpa.split('#')[0].strip()
             if parte_util:
-                RAM[ponteiro_instr] = parte_util
+                RAM[ponteiro_instr] = int(parte_util)
                 ponteiro_instr += 1
 
 def exibir_registradores():
     # Função para exibir o estado atual dos registradores na tela inicial do usuário
     print('ESTADO ATUAL DOS REGISTRADORES')
-    print(f"Gerais -> A: {REGS_GERAIS['A']} | B: {REGS_GERAIS['B']}")
+    print(f"Gerais   -> A: {REGS_GERAIS['A']} | B: {REGS_GERAIS['B']}")
     print(f"Controle -> PC: 0x{REGS_ESPECIAIS['PC']:02X} | IR: '{REGS_ESPECIAIS['IR']}'")
-    print(f"Memória -> MAR: 0x{REGS_ESPECIAIS['MAR']:02X} | MBR: {REGS_ESPECIAIS['MBR']}")
-    print(f"ULA -> AC: {REGS_ESPECIAIS['AC']} | M: {REGS_ESPECIAIS['M']} | R: {REGS_ESPECIAIS['R']}")
-    print(f"Flags -> C: {REGS_ESPECIAIS['C']} | N: {REGS_ESPECIAIS['N']} | Z: {REGS_ESPECIAIS['Z']}")
+    print(f"Memória  -> MAR: 0x{REGS_ESPECIAIS['MAR']:02X} | MBR: {REGS_ESPECIAIS['MBR']}")
+    print(f"ULA      -> AC: {REGS_ESPECIAIS['AC']} | M: {REGS_ESPECIAIS['M']} | R: {REGS_ESPECIAIS['R']}")
+    print(f"Flags    -> C: {REGS_ESPECIAIS['C']} | N: {REGS_ESPECIAIS['N']} | Z: {REGS_ESPECIAIS['Z']}")
     print()
 
     input('Pressione <ENTER> para avançar para o próximo ciclo...')
 
-def executar_comando(instrucao: str): #LOAD A, 0x05
+def executar_comando(instrucao: str):
     '''Decodifica a string da instrução e altera os registradores ou a RAM.'''
     
     global RAM, REGS_GERAIS, REGS_ESPECIAIS
@@ -149,6 +149,60 @@ def executar_comando(instrucao: str): #LOAD A, 0x05
    
         RAM[indice_ram] = REGS_GERAIS[reg_origem]
         print(f"[ULA] Sucesso: RAM[{endereco_hex}] agora guarda o valor {REGS_GERAIS[reg_origem]}")
+
+    elif opcode == 'SUB':
+        reg_atual = partes[1]
+        endereco_hex = partes[2]
+        
+        indice_ram = int(endereco_hex, 16)
+        valor_ram = RAM[indice_ram]
+        
+        resultado = REGS_GERAIS[reg_atual] - valor_ram
+        REGS_ESPECIAIS['AC'] = resultado
+        REGS_GERAIS[reg_atual] = resultado
+        
+        REGS_ESPECIAIS['Z'] = 1 if resultado == 0 else 0
+        REGS_ESPECIAIS['N'] = 1 if resultado < 0 else 0
+        print(f"[ULA] SUB: Subtraído {valor_ram} do Registrador {reg_atual}. AC = {resultado}")
+
+    elif opcode == 'DIV':
+        reg_atual = partes[1]
+        endereco_hex = partes[2]
+        
+        indice_ram = int(endereco_hex, 16)
+        valor_ram = RAM[indice_ram]
+        
+        if valor_ram == 0:
+            print("[Erro ULA] Divisão por zero!")
+            return
+            
+        quociente = REGS_GERAIS[reg_atual] // valor_ram
+        resto = REGS_GERAIS[reg_atual] % valor_ram
+        
+        REGS_ESPECIAIS['AC'] = quociente
+        REGS_ESPECIAIS['R'] = resto
+        REGS_GERAIS[reg_atual] = quociente
+        
+        REGS_ESPECIAIS['Z'] = 1 if quociente == 0 else 0
+        REGS_ESPECIAIS['N'] = 1 if quociente < 0 else 0
+        print(f"[ULA] DIV: {reg_atual} / {valor_ram}. AC (Quociente) = {quociente}, R (Resto) = {resto}")
+
+    elif opcode == 'JUMP':
+        endereco_hex = partes[1]
+        indice_destino = int(endereco_hex, 16)
+        
+        REGS_ESPECIAIS['PC'] = indice_destino
+        print(f"[Controle] JUMP: Fluxo desviado incondicionalmente para 0x{indice_destino:02X}")
+
+    elif opcode == "JUMP+":
+        endereco_hex = partes[1]
+        indice_destino = int(endereco_hex, 16)
+        
+        if REGS_ESPECIAIS['AC'] >= 0:
+            REGS_ESPECIAIS['PC'] = indice_destino
+            print(f"[Controle] JUMP+: AC é {REGS_ESPECIAIS['AC']} (positivo). Desviando para 0x{indice_destino:02X}")
+        else:
+            print(f"[Controle] JUMP+: AC é {REGS_ESPECIAIS['AC']} (negativo). Desvio NÃO realizado.")
 
 def ciclo_instrucao():
     exibir_registradores()
