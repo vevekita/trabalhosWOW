a67d51b119005f6da7f64ea933b2911cd0856f93
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-27 19:08:48 -0300

    Refactor Prontuario class for JPA integration

--- trabalhoPOO2.0/src/clinica/Prontuario.java
+++ trabalhoPOO2.0/src/clinica/Prontuario.java
@@ -3,6 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package clinica;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 /**
  * Classe que armazena as informações clínicas de um paciente:
  * -IdPaciente (para associação à uma classe Paciente)
@@ -11,10 +18,19 @@ package clinica;
  * -Prescrição
  * 
  */
+@Entity
+@Table(name="PRONTUÁRIOS")
 public class Prontuario {
+    // Pojo para representar um objeto do tipo Prontuario com id do paciente, sintomas, diagnostico e prescrição.
+    // Este Pojo será mapeado em uma tabela chamada PRONTUÁRIOS no banco de dados.
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int idPaciente;
+    @Column(length=100, name = "SINTOMAS", nullable = false)
     private String sintomas;
+    @Column(length=50, name = "DIAGNÓSTICO", nullable = false)
     private String diagnostico;
+    @Column(length=50, name = "PRESCRIÇÃO", nullable = false)
     private String prescricao;
     
     public Prontuario(){}
