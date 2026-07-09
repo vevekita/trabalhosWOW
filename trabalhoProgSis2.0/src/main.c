351b036bdf666da9d441b123557ee562433ef5f4
NanaYaguti <annalisiayaguti@gmail.com>
2026-07-08 22:09:10 -0300

    add menu

--- trabalhoProgSis2.0/src/main.c
+++ trabalhoProgSis2.0/src/main.c
@@ -24,6 +24,8 @@ struct point {
 };
 struct point fruit;
 int velocity_apple = 0;
+char textScore[11] = "SCORE:0000";
+char textTime[9] = "TIME:000";
 
 // retorna cor do pixel (x,y)
 int pget(int x, int y) {
@@ -35,6 +37,7 @@ int pget(int x, int y) {
 }
 
 int x, y; // posição do jogador
+int esquerda;
 
 void spawn_fruit() {
     fruit.x = rand()%153;
@@ -57,113 +60,184 @@ void start () {
 void update () {
     frame++;
     
-    if (jogo == MENU) { //se está no modo de menu/começo do jogo
-        *DRAW_COLORS = 0x1;
-        rect(0, 0, 160, 160); 
-        text("aperte [ENTER] para começar o jogo", 45, 62);
-
-    }
-    //aqui tudo entra dentro do if jogo = JOGO. Também tem o if jogo = FIM
-    if (fruit.stop>0) { //faz contagem regressiva para a maçã não cair
-        fruit.stop--;
-    } else {
-        if (frame % 2 == 0) {
-            fruit.y++;  
-        }
-    }
-    if (fruit.y > 110) { //se a maçã cair no chão
-        spawn_fruit(); //spawna outra fruta
-    }
-
-    //background
-    *DRAW_COLORS = 0x1234;
-    blit(background_jogo, 0, 0, background_jogoWidth, background_jogoHeight, background_jogoFlags);
-    
-    if (timer <= 0) {
-        jogo = FIM;
-        *DRAW_COLORS = 0x12;
-        rect(35, 50, 92, 30);
+    if (jogo == MENU) {
+        *DRAW_COLORS = 0x3;
+        rect(0, 0, 160, 160);
         *DRAW_COLORS = 0x0001;
-        text("GAME OVER", 45, 62);
-        tone(100, 40, 100, 2);
-        return;
-    } else {
-        timer--;
-    }
+        text("PONY GAME", 45, 20);
 
-    //parte do input
-    srand((unsigned int)frame);
-    int dx = 0; // deslocamento, permite mais de uma tecla pressionada
-    int esquerda = 0;
-    uint8_t gamepad = *GAMEPAD1;
-    if (gamepad & BUTTON_LEFT)  { 
-        dx -= 1;
-        esquerda = 1;
-    }
-    if (gamepad & BUTTON_RIGHT) { 
-        dx += 1; 
-    }
-    x += dx;
-    
-    int frameAtual = 0;
-    if (x < -2) {
-        x = -2;
-    }
+        *DRAW_COLORS = 0x4;
+        rect(10, 40, 140, 45);
+        *DRAW_COLORS = 0x2;
+        line(150, 85, 150, 40);
+        line(10, 85, 150, 85);
 
-    if (x > 150) {
-        x = 150;
-    }
-    if (dx != 0) {
-        frameAtual = ((frame / 12) % 2) + 1;
-    }
-    
-    int spriteX = frameAtual * 16;
-    int spriteY = 0;
+        *DRAW_COLORS = 0x0001;
+        text("-> bad apple", 40, 70);
+        *DRAW_COLORS = 0x0123;
+        blitSub(ponyBehind, 20, 66, 16, 16, (uint32_t)64, 0, ponyBehindWidth, BLIT_2BPP);
 
-    //applejack (pony)
-    *DRAW_COLORS = 0x0024;
-    blitSub(ponyBehind, x, y, 16, 16, (uint32_t)spriteX, (uint32_t)spriteY, ponyBehindWidth, BLIT_2BPP);
-    *DRAW_COLORS = 0x0123;
-    blitSub(pony, x, y, 16, 16, (uint32_t)spriteX, (uint32_t)spriteY, ponyWidth, BLIT_2BPP);
-    
-    if (x < fruit.x + 5 && x + 5 > fruit.x && y < fruit.y + 5 && y + 5 > fruit.y) { //se colidiu
-        if (fruit.type == GOOD_APPLE) {
-            score += 10;
-            tone(800, 20, 100, TONE_PULSE1);
+        *DRAW_COLORS = 0x0001;
+        text("-> good apple", 40, 45);
+        *DRAW_COLORS = 0x0124; 
+        blitSub(ponyBehind, 20, 42, 16, 16, (uint32_t)48, 0, ponyBehindWidth, BLIT_2BPP);
+        *DRAW_COLORS = 0x0123;
+        blitSub(pony, 20, 42, 16, 16, (uint32_t)48, 0, ponyWidth, BLIT_2BPP);
+
+        *DRAW_COLORS = 0x0001;
+        text("Try to catch all the", 0, 95);
+        text("good apples in the time limit", 10, 105);
+        text("time limit", 40, 115);
+
+        *DRAW_COLORS = 0x4;
+        rect(5, 127, 150, 14);
+        *DRAW_COLORS = 0x2;
+        line(5, 141, 155, 141);
+        line(155, 127, 155, 141);
+
+        *DRAW_COLORS = 0x0001;
+        text("press [X] to start the game", 10, 130);
+        uint8_t gamepad = *GAMEPAD1;
+        if (gamepad & BUTTON_1) {
+            jogo = JOGO;
+        }
+
+    }
+    else if (jogo == JOGO) {
+        if (fruit.stop>0) { //faz contagem regressiva para a maçã não cair
+            fruit.stop--;
         } else {
-            if (score > 0) {
-                score -= 10;
+            if (frame % 2 == 0) {
+                fruit.y++;  
             }
-            
-            tone(120, 25 << 16, 90, 3);
+        }
+        if (fruit.y > 110) { //se a maçã cair no chão
+            spawn_fruit(); //spawna outra fruta
+        }
+
+        //background
+        *DRAW_COLORS = 0x1234;
+        blit(background_jogo, 0, 0, background_jogoWidth, background_jogoHeight, background_jogoFlags);
+        
+        if (timer <= 0) {
+            jogo = FIM;
+        } else {
+            timer--;
+        }
+
+        //parte do input
+        srand((unsigned int)frame);
+        int dx = 0; // deslocamento, permite mais de uma tecla pressionada
+        uint8_t gamepad = *GAMEPAD1;
+        if (gamepad & BUTTON_LEFT)  { 
+            dx -= 1;
+            esquerda = 1;
+        }
+        if (gamepad & BUTTON_RIGHT) { 
+             dx += 1;
+            esquerda = 0; 
+        }
+        x += dx;
+        
+        int frameAtual = 0;
+        if (x < -2) {
+            x = -2;
+        }
+
+        if (x > 150) {
+            x = 150;
+        }
+        if (dx != 0) {
+            frameAtual = ((frame / 12) % 2) + 1;
         }
         
-        spawn_fruit();
-    }
+        int spriteX = frameAtual * 16;
+        int spriteY = 0;
+        uint32_t flags_desenho = BLIT_2BPP;
+        if (esquerda) {
+            flags_desenho |= BLIT_FLIP_X;
+        }
+    
 
-    if (fruit.type == BAD_APPLE) {
+        //applejack (pony)
+        *DRAW_COLORS = 0x0024;
+        blitSub(ponyBehind, x, y, 16, 16, (uint32_t)spriteX, (uint32_t)spriteY, ponyBehindWidth, flags_desenho);
         *DRAW_COLORS = 0x0123;
-        blitSub(ponyBehind, fruit.x, fruit.y, 16, 16, (uint32_t)64, (uint32_t)spriteY, ponyBehindWidth, BLIT_2BPP);
-    } else {
-        blitSub(ponyBehind, fruit.x, fruit.y, 16, 16, (uint32_t)48, (uint32_t)spriteY, ponyBehindWidth, BLIT_2BPP);
-        blitSub(pony, fruit.x, fruit.y, 16, 16, (uint32_t)48, (uint32_t)spriteY, ponyWidth, BLIT_2BPP);
+        blitSub(pony, x, y, 16, 16, (uint32_t)spriteX, (uint32_t)spriteY, ponyWidth, flags_desenho);        
+        if (x < fruit.x + 5 && x + 5 > fruit.x && y < fruit.y + 5 && y + 5 > fruit.y) { //se colidiu
+            if (fruit.type == GOOD_APPLE) {
+                score += 10;
+                tone(800, 20, 100, TONE_PULSE1);
+            } else {
+                if (score > 0) {
+                    score -= 10;
+                }
+                
+                tone(120, 25 << 16, 90, 3);
+            }
+            
+            spawn_fruit();
+        }
+
+        if (fruit.type == BAD_APPLE) {
+            *DRAW_COLORS = 0x0123;
+            blitSub(ponyBehind, fruit.x, fruit.y, 16, 16, (uint32_t)64, (uint32_t)spriteY, ponyBehindWidth, BLIT_2BPP);
+        } else {
+            *DRAW_COLORS = 0x0124; 
+            blitSub(ponyBehind, fruit.x, fruit.y, 16, 16, (uint32_t)48, (uint32_t)spriteY, ponyBehindWidth, BLIT_2BPP);
+            *DRAW_COLORS = 0x0123;
+            blitSub(pony, fruit.x, fruit.y, 16, 16, (uint32_t)48, (uint32_t)spriteY, ponyWidth, BLIT_2BPP);
+        }
+
+        *DRAW_COLORS = 0x12;
+        rect(0, 125, 160, 35);
+
+        char textScore[11] = "SCORE:0000";
+        char textTime[9] = "TIME:000";
+        textScore[9] = (char)('0' + score % 10);
+        textScore[8] = (char)('0' + ((score / 10) % 10));
+        textScore[7] = (char)('0' + ((score / 100) % 10));
+        textScore[6] = (char)('0' + ((score / 1000) % 10));
+        *DRAW_COLORS = 0x0001;
+        text(textScore, 4, 140);
+
+        int timerSec= timer / 60;
+        textTime[7] = (char)('0' + timerSec % 10); //calcula as unidades
+        textTime[6] = (char)('0' + ((timerSec / 10) % 10));//calcula as dezenas
+        textTime[5] = (char)('0' + ((timerSec / 100) % 10)); //calcula as centenas
+        text(textTime, 90, 140);
+
     }
+    else if (jogo == FIM) {
+        *DRAW_COLORS = 0x1234;
+        blit(background_jogo, 0, 0, background_jogoWidth, background_jogoHeight, background_jogoFlags);
 
-    *DRAW_COLORS = 0x12;
-    rect(0, 125, 160, 35);
+        *DRAW_COLORS = 0x13;
+        rect(20, 50, 120, 100);
+        *DRAW_COLORS = 0x2;
+        text("GAME OVER", 46, 64);
+        *DRAW_COLORS = 0x0004;
+        text("GAME OVER", 45, 62);
+        *DRAW_COLORS = 0x0001;
+        text(textScore, 42, 85);
 
-    char textScore[11] = "SCORE:0000";
-    textScore[9] = (char)('0' + score % 10);
-    textScore[8] = (char)('0' + ((score / 10) % 10));
-    textScore[7] = (char)('0' + ((score / 100) % 10));
-    textScore[6] = (char)('0' + ((score / 1000) % 10));
-    *DRAW_COLORS = 0x0001;
-    text(textScore, 4, 140);
+        *DRAW_COLORS = 0x4;
+        rect(27, 105, 106, 35);
+        *DRAW_COLORS = 0x2;
+        line(27, 140, 133, 140);
+        line(133, 105, 133, 140);
+        *DRAW_COLORS = 0x0001;
+        text("PRESS [Z] TO", 33, 110);
+        text("GO BACK TO", 42, 120);
+        text("MENU", 65, 130);
 
-    int timerSec= timer / 60;
-    char textTime[9] = "TIME:000";
-    textTime[7] = (char)('0' + timerSec % 10); //calcula as unidades
-    textTime[6] = (char)('0' + ((timerSec / 10) % 10));//calcula as dezenas
-    textTime[5] = (char)('0' + ((timerSec / 100) % 10)); //calcula as centenas
-    text(textTime, 90, 140);
+        uint8_t gamepad = *GAMEPAD1;
+        if (gamepad & BUTTON_2) {
+            timer = 3600;
+            score = 0;
+            spawn_fruit();
+            jogo = MENU;
+        }
+    }
+    
 }
